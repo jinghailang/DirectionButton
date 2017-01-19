28d37e95293b28814d96244d031ecda8e39b83a5
zengtao <895989112@qq.com>
2017-01-19 14:12:11 +0800

    Initial Commit

---
+++ ButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonDemo
+//
+//  Created by litao on 17/1/19.
+//  Copyright © 2017年 zengtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonDemo
+//
+//  Created by litao on 17/1/19.
+//  Copyright © 2017年 zengtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
